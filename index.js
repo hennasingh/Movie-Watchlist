12d229bd49baf5da5f90021f2fae46db283c664e
Henna Singh <hennasingh04@gmail.com>
2023-11-22 19:24:39 +0000

    implemented code to handle undefined results and added watchlist page. improved styles and error handling

--- index.js
+++ index.js
@@ -1,44 +1,71 @@
-const searchTextForm = document.getElementById('search-text-form')
-const containerMovies = document.getElementById('movies')
+const searchTextForm = document.getElementById("search-text-form");
+const containerMovies = document.getElementById("movies");
 
-searchTextForm.addEventListener('submit', getSearchText)
-let movieDetails = ``
 
+let movieDetails = ``;
+const url = window.location.href
+
+console.log(url)
+if(url.endsWith('index.html')){ 
+    searchTextForm.addEventListener("submit", getSearchText);
+} else {
+    console.log('watchlist')
+}
 function getSearchText(event) {
-    event.preventDefault()
+  event.preventDefault();
 
-    const formData = new FormData(searchTextForm)
-    const searchText = formData.get('search-text')
-    findMovies(searchText.trim())
+  const formData = new FormData(searchTextForm);
+  const searchText = formData.get("search-text");
+  findMovies(searchText.trim());
 }
 
 async function findMovies(searchText) {
-    if(searchText) {
 
-        const response = await fetch(`http://www.omdbapi.com/?apikey=a813525a&s=${searchText}`)
-        const data = await response.json()
-
-        getSearchResults(data.Search)
+  if (searchText) {
+    try {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=a813525a&s=${searchText}`
+      );
+      const data = await response.json();
+      if (data.Search == undefined) {
+        throw Error("Unable to find what you're looking for");
+      } else {
+        getSearchResults(data.Search);
+      }
+    } catch (err) {
+      {
+        containerMovies.innerHTML = `
+            <div class="no-movies">
+                <p >Unable to find what you're looking for. Please try another search.</p>
+            </div>      
+            `
+      }
     }
+  }
 }
 
 function getSearchResults(movies) {
-    
-    movies.forEach(movie => {
-        displayMovieDetails(movie.imdbID)
-    })
+  movies.forEach((movie) => {
+    displayMovieDetails(movie.imdbID);
+  });
 }
 
 async function displayMovieDetails(movieId) {
-    const response = await fetch(`http://www.omdbapi.com/?apikey=a813525a&i=${movieId}`)
-    const movie = await response.json()
-        movieDetails += `
+  const response = await fetch(
+    `http://www.omdbapi.com/?apikey=a813525a&i=${movieId}`
+  );
+  const movie = await response.json();
+  movieDetails += `
             <div class="card">
                 <div class="poster">
-                    <img src=${movie.Poster != "N/A" ? movie.Poster: "No-Image.png"} class="movie-poster">
+                    <img src=${
+                      movie.Poster != "N/A" ? movie.Poster : "No-Image.png"
+                    } class="movie-poster">
                 </div>
                 <div class="details">
-                    <h3>${movie.Title}<img src="./star.png" class="rating-star"/><span class="rating">${movie.imdbRating}</span></h3>                   
+                    <h3>
+                    ${movie.Title}
+                    <img src="./star.png" class="rating-star"/><span class="rating">${movie.imdbRating}</span></h3>                   
                     <div class="movie-props">
                         <p>${movie.Runtime}</p>
                         <p>${movie.Genre}</p>
@@ -47,8 +74,7 @@ async function displayMovieDetails(movieId) {
                     <p class="movie-plot">${movie.Plot}</p>
                 </div>
             </div>
-        `
-      //console.log(movieDetails)
-      containerMovies.innerHTML = movieDetails
-   
+            `
+  //console.log(movieDetails)
+  containerMovies.innerHTML = movieDetails;
 }
