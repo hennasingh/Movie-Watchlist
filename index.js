f0eafc3ac4f0ca0a182ef7a4971b2349721a25b0
Henna Singh <hennasingh04@gmail.com>
2023-11-24 09:42:59 +0000

    implemented local storage to save movies and remove on clicking image. Edited styles and updated watchlist.html

--- index.js
+++ index.js
@@ -1,22 +1,60 @@
-const searchTextForm = document.getElementById("search-text-form");
-const containerMovies = document.getElementById("movies");
+const searchTextForm = document.getElementById("search-text-form")
+const containerMovies = document.getElementById("movies")
 
 
-let movieDetails = ``;
+let movieDetails = ``
+let mySavedMovies = []
 const url = window.location.href
+const moviesFromStorage = JSON.parse(localStorage.getItem("myMovies"))
 
-console.log(url)
+if(moviesFromStorage){
+    mySavedMovies = moviesFromStorage
+}
+
+document.addEventListener('click', function(event) {
+    if(event.target.dataset.add){
+        if(!mySavedMovies.includes(event.target.dataset.add)){
+            mySavedMovies.push(event.target.dataset.add)
+            localStorage.setItem("myMovies", JSON.stringify(mySavedMovies))
+        }
+    } else if(event.target.dataset.remove){
+        console.log(event.target.dataset.remove)
+        const value = event.target.dataset.remove
+        if(mySavedMovies.length == 1){
+            movieDetails = ``
+            containerMovies.innerHTML = `
+                <div class="start-exploring">
+                    <p class="no-movies"> Your watchlist is looking a little empty...</p>
+                    <a href="index.html" class="bold add-movie"><img src="./add.png" /> Let's add some movies</a>
+                </div>
+                `
+        } else {
+            mySavedMovies = mySavedMovies.filter(item => item!== value)
+        
+            localStorage.setItem("myMovies",JSON.stringify(mySavedMovies) )
+            renderMovies()
+        }
+    }
+})
+
 if(url.endsWith('index.html')){ 
     searchTextForm.addEventListener("submit", getSearchText);
 } else {
     console.log('watchlist')
+    renderMovies()
+}
+function renderMovies() {
+    movieDetails = ``
+    mySavedMovies.forEach(movieId => {
+        displayMovieDetails(movieId)
+    })
 }
 function getSearchText(event) {
-  event.preventDefault();
+  event.preventDefault()
 
   const formData = new FormData(searchTextForm);
-  const searchText = formData.get("search-text");
-  findMovies(searchText.trim());
+  const searchText = formData.get("search-text")
+  findMovies(searchText.trim())
 }
 
 async function findMovies(searchText) {
@@ -26,11 +64,11 @@ async function findMovies(searchText) {
       const response = await fetch(
         `http://www.omdbapi.com/?apikey=a813525a&s=${searchText}`
       );
-      const data = await response.json();
+      const data = await response.json()
       if (data.Search == undefined) {
-        throw Error("Unable to find what you're looking for");
+        throw Error("Unable to find what you're looking for")
       } else {
-        getSearchResults(data.Search);
+        getSearchResults(data.Search)
       }
     } catch (err) {
       {
@@ -46,15 +84,16 @@ async function findMovies(searchText) {
 
 function getSearchResults(movies) {
   movies.forEach((movie) => {
-    displayMovieDetails(movie.imdbID);
-  });
+    displayMovieDetails(movie.imdbID)
+  })
 }
 
 async function displayMovieDetails(movieId) {
   const response = await fetch(
     `http://www.omdbapi.com/?apikey=a813525a&i=${movieId}`
-  );
-  const movie = await response.json();
+  )
+  const movie = await response.json()
+  const image = url.endsWith('index.html') ? `<img src="./add.png" data-add=${movieId} />Watchlist`: `<img src="./remove.png" data-remove=${movieId} />Remove`
   movieDetails += `
             <div class="card">
                 <div class="poster">
@@ -69,12 +108,11 @@ async function displayMovieDetails(movieId) {
                     <div class="movie-props">
                         <p>${movie.Runtime}</p>
                         <p>${movie.Genre}</p>
-                        <p class="add-movie"><img src="./add.png" data-add=${movieId} />Watchlist</p>
+                        <p class="add-movie">${image}</p>
                     </div>
                     <p class="movie-plot">${movie.Plot}</p>
                 </div>
             </div>
             `
-  //console.log(movieDetails)
-  containerMovies.innerHTML = movieDetails;
+  containerMovies.innerHTML = movieDetails
 }
